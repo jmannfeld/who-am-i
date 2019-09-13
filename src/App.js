3ef8aea7ca3d8bf2879584803bfaa82aaf62d6b1
Jordan Mannfeld <jordanmannfeld@gmail.com>
2019-09-13 19:50:12 -0400

    adding default mystery avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import MysteryTile from './MysteryTile';
-const saveSvgAsPng = require('save-svg-as-png');
+import mysteryPerson from './images/mystery-person.jpg';
+
+const { saveSvgAsPng } = require('save-svg-as-png');
 
 function App() {
   const [borderColor, setBorderColor] = useState("#FF43E3");
   const [backgroundColor, setBackgroundColor] = useState("#FAD0EA");
   const [name, setName] = useState("Mr/Ms X");
+  const [image, setImage] = useState(mysteryPerson);
   const [factOne, setFactOne] = useState(null);
   const [factTwo, setFactTwo] = useState(null);
   const [factThree, setFactThree] = useState(null);
@@ -21,7 +24,13 @@ function App() {
   ]
 
   function downloadSvg() {
-    saveSvgAsPng.saveSvgAsPng(document.getElementById("mystery-tile"), "mystery.png");
+    const slide = document.getElementById("mystery-tile");
+    saveSvgAsPng(slide, "mystery.png");
+  }
+
+  function transformImage(target) {
+    URL.createObjectURL(target.files[0])
+    setImage(URL.createObjectURL(target.files[0]))
   }
 
   function addButtonHighlight(button) {
@@ -36,43 +45,56 @@ function App() {
     <div className="App">
       <MysteryTile
         name={name}
+        image={image}
         facts={facts}
         borderColor={borderColor}
         backgroundColor={backgroundColor}
       />
       <div id="input-list">
-        <button onClick={downloadSvg} onMouseEnter={(e) => addButtonHighlight(e.target)} onMouseLeave={(e) => removeButtonHighlight(e.target)}>Download PNG</button>
-        <div className="name">
-          <label htmlFor="name">Name:</label>
-          <input onChange={(e) => setName(e.target.value)} type="text" name="name" id="name"/>
-        </div>
-        <div className="input-field split">
-          <div className="input-field">
+        <button
+          onClick={downloadSvg}
+          onMouseEnter={(e) => addButtonHighlight(e.target)}
+          onMouseLeave={(e) => removeButtonHighlight(e.target)}
+        >
+          Download PNG
+        </button>
+        <div className="field split colors">
+          <div className="field">
             <label htmlFor="border-color">Highlight Color:</label>
             <input onChange={(e) => setBorderColor(e.target.value)} value={borderColor} type="color" name="border-color" id="border-color"/>
           </div>
-          <div className="input-field">
+          <div className="field">
             <label htmlFor="background-color">Background Color:</label>
             <input onChange={(e) => setBackgroundColor(e.target.value)} value={backgroundColor} type="color" name="background-color" id="background-color"/>
           </div>
         </div>
-        <div className="input-field">
+        <div className="field split person-details">
+          <div className="name">
+            <label htmlFor="name">Name:</label>
+            <input onChange={(e) => setName(e.target.value)} type="text" name="name" id="name"/>
+          </div>
+          <div className="image">
+            <label htmlFor="image-upload" className="custom-file-upload">Upload Image</label>
+            <input onChange={(e) => transformImage(e.target)} type="file" name="image-upload" id="image-upload"/>
+          </div>
+        </div>
+        <div className="field">
           <label htmlFor="fact-1">Fact 1:</label>
           <input onChange={(e) => setFactOne(e.target.value)} type="text" name="fact-1" id="fact-1"/>
         </div>
-        <div className="input-field">
+        <div className="field">
           <label htmlFor="fact-2">Fact 2:</label>
           <input onChange={(e) => setFactTwo(e.target.value)} type="text" name="fact-2" id="fact-2"/>
         </div>
-        <div className="input-field">
+        <div className="field">
           <label htmlFor="fact-3">Fact 3:</label>
           <input onChange={(e) => setFactThree(e.target.value)} type="text" name="fact-3" id="fact-3"/>
         </div>
-        <div className="input-field">
+        <div className="field">
           <label htmlFor="fact-4">Fact 4:</label>
           <input onChange={(e) => setFactFour(e.target.value)} type="text" name="fact-4" id="fact-4"/>
         </div>
-        <div className="input-field">
+        <div className="field">
           <label htmlFor="fact-5">Fact 5:</label>
           <input onChange={(e) => setFactFive(e.target.value)} type="text" name="fact-5" id="fact-5"/>
         </div>
--- src/MysteryTile.js
+++ src/MysteryTile.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css';
 
-const MysteryTile = ({name, facts, borderColor, backgroundColor}) => {
+const MysteryTile = ({name, image, facts, borderColor, backgroundColor}) => {
   return (
-    <svg id="mystery-tile" width="640px" height="360px" xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <svg id="mystery-tile" width="640px" height="360px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
       <rect width="100%" height="100%" fill={backgroundColor} stroke={borderColor} strokeWidth="25"></rect>
-      <rect width="30%" height="65%" fill="white" stroke={borderColor} strokeWidth="3" rx="5" x="45" y="45"></rect>
+			<defs>
+				<mask id="shape-mask">
+          <rect width="30%" height="65%" fill="white" stroke={borderColor} strokeWidth="3" rx="5" x="45" y="45"></rect>
+				</mask>
+			</defs>
+			<image width="30%" height="65%" x="45" y="45" preserveAspectRatio="xMidYMid slice" xlinkHref={image} mask="url(#shape-mask)"></image>
       <text width="30%" x="90" y="315" fontSize="24" fontWeight="bold" fill={borderColor}>{name}</text>
       <g>
         <text x="355" y="65" fontSize="32" fontWeight="bold" fontStyle="italic" fill={borderColor}>Who Am I?</text>
